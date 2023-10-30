09527586f8618b2b625c24fb7d9a206b98aad6db
Gigachad473 <ilahristoforov88@gmail.com>
2023-10-30 12:53:27 +0100

    New commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.redirect('https://preppal.cyclic.app/room-cooking-pot'); 
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');  
+});
