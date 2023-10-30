fad04137c833dc74e44f7b0d87ee98e34582a49d
Gigachad473 <ilahristoforov88@gmail.com>
2023-10-30 15:00:58 +0100

    New commit

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.redirect('https://preppal.cyclic.app/room-cooking-pot'); 
+  const ua = req.get('user-agent');
+  const isInstagram = ua.indexOf('Instagram') > -1;
+  
+  if (isInstagram) {
+    if (/iPad|iPhone|iPod/.test(ua)) {
+      res.redirect(302, 'googlechrome://preppal.cyclic.app/room-cooking-pot');
+      return;
+    }
+    res.redirect(302, 'intent:https://preppal.cyclic.app/room-cooking-pot#Intent;end');
+    return;
+  }
+
+  // Handle other cases or provide a default redirect if not using Instagram.
+  res.redirect(302, 'https://preppal.cyclic.app/room-cooking-pot');
 });
 
 app.listen(process.env.PORT || 3000, () => {
